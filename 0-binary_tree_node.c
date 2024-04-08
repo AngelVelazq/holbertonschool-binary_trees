81d447805a7e9aacebdd837af1d3f9268d8fcca4
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-04-08 10:27:42 -0400

    pushing = in the binary tree i put all of the 18 protypes in the header file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,3 +1,10 @@
 #include "binary_trees.h"
 /**
- * binary_trees_node - 
+ * binary_trees_node - creates a binary tree
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to a new node, NULL on fail
+**/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
