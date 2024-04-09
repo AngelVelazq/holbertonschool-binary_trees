da2c78a56e74da41b778b8fb7ca8d2ed30863903
AngelVelazq <angelvelazquez.op@gmail.com>
2024-04-09 12:53:48 -0400

    checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,6 +23,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+
+	if (tree == NULL)
+		return (0);
+
 	size_t height = binary_tree_height(tree);
 	size_t nodes = binary_tree_size(tree);
 
@@ -38,8 +42,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
